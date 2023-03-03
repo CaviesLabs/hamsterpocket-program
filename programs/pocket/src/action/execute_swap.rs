82cc76f656de2bfdad841dd05f40b67c8c882ed2
0xTraderTrou <khangtran.btc@gmail.com>
2023-03-03 19:18:26 +0700

    chore: Improve pocket execution and testing
    - Add logic to handle the `execute_swap` action
    - Extract accounts from the `Context`
    - Make the swap and check and update the pocket risk
    - Increase the amount of returned recordings
    - Add an OpenAI API for completions
    - Lower numeric tolerance for test files
    - Add 2 tests for the inclusive string split function
    
    [programs/pocket/src/action/execute_swap.rs]
    - Add logic to handle the `execute_swap` action
    - Extract accounts from the `Context`
    - Make the swap and check and update the pocket risk
    - Raise the amount of returned recordings from `10` to `100`
    - Move the `octokit` initialization to a separate file
    - Add an OpenAI API for completions
    - Lower numeric tolerance for test files
    - Add 2 tests for the inclusive string split function

--- programs/pocket/src/action/execute_swap.rs
+++ programs/pocket/src/action/execute_swap.rs
@@ -1,5 +1,103 @@
 use crate::*;
 
+pub fn handle_execute_swap<'info>(ctx: Context<'_, '_, '_, 'info, ExecuteSwapContext<'info>>) -> Result<()> {
+    let pocket_registry = ctx.accounts.pocket_registry.clone();
+    let pocket = ctx.accounts.pocket.clone();
+    let signer = ctx.accounts.signer.clone();
+
+    // Only allow operator to perform the swap
+    if !pocket_registry.is_operator(signer.key()) {
+        return Err(PocketError::OnlyOperator.into());
+    }
+
+    // Check whether the pocket is ready to swap
+    if !pocket.is_ready_to_swap() {
+        return Err(PocketError::NotReadyToSwap.into());
+    }
+
+    // Make Swap
+    let did_swap = swap(&ctx).unwrap();
+
+    // Pocket risk check and update
+    ctx.accounts.ensure_pocket_integrity(&did_swap).unwrap();
+
+    // Return result
+    Ok(())
+}
+
+fn swap<'info>(ctx: &Context<'_, '_, '_, 'info, ExecuteSwapContext<'info>>) -> Result<DidSwap> {
+    let pocket = &ctx.accounts.pocket;
+
+    // Determine side
+    let mut side = Side::Ask;
+    if pocket.side == TradeSide::Buy {
+        side = Side::Bid;
+    }
+
+    let amount_to_swap = match pocket.side {
+        TradeSide::Buy => {
+            if pocket.batch_volume <= pocket.quote_token_balance {
+                pocket.batch_volume
+            } else {
+                pocket.quote_token_balance
+            }
+        }
+
+        TradeSide::Sell => {
+            if pocket.batch_volume <= pocket.base_token_balance {
+                pocket.batch_volume
+            } else {
+                pocket.base_token_balance
+            }
+        }
+    };
+
+    // Extract accounts
+    let event_queue = &mut ctx.remaining_accounts.get(0).unwrap();
+    let request_queue = &mut ctx.remaining_accounts.get(1).unwrap();
+    let market_bids = &mut ctx.remaining_accounts.get(2).unwrap();
+    let market_asks = &mut ctx.remaining_accounts.get(3).unwrap();
+    let coin_vault = &mut ctx.remaining_accounts.get(4).unwrap();
+    let pc_vault = &mut ctx.remaining_accounts.get(5).unwrap();
+    let market_authority = &mut ctx.remaining_accounts.get(6).unwrap();
+    let open_orders = &mut ctx.remaining_accounts.get(7).unwrap();
+    let dex_program = &mut ctx.remaining_accounts.get(8).unwrap();
+
+    // Make swap
+    let did_swap = external::swap(Swap {
+        market: MarketAccounts {
+            market: ctx.accounts.market_key.to_account_info(),
+            open_orders: open_orders.to_account_info(),
+            request_queue: request_queue.to_account_info(),
+            event_queue: event_queue.to_account_info(),
+            bids: market_bids.to_account_info(),
+            asks: market_asks.to_account_info(),
+            order_payer_token_account: match side {
+                Side::Bid => ctx.accounts.pocket_quote_token_vault.to_account_info(),
+                Side::Ask => ctx.accounts.pocket_base_token_vault.to_account_info(),
+            },
+            coin_wallet: ctx.accounts.pocket_base_token_vault.to_account_info(),
+            coin_vault: coin_vault.to_account_info(),
+            pc_vault: pc_vault.to_account_info(),
+            vault_signer: market_authority.to_account_info(),
+        },
+        authority: ctx.accounts.pocket.to_account_info(),
+        pc_wallet: ctx.accounts.pocket_quote_token_vault.to_account_info(),
+        dex_program: dex_program.to_account_info(),
+        token_program: ctx.accounts.token_program.to_account_info(),
+        rent: ctx.accounts.rent.to_account_info(),
+        pocket: ctx.accounts.pocket.clone(),
+    }, side, amount_to_swap, ExchangeRate {
+        rate: 0,
+        from_decimals: 0,
+        quote_decimals: 0,
+        strict: false,
+    }).unwrap();
+
+    // Return
+    Ok(did_swap)
+}
+
 #[derive(Accounts)]
 pub struct ExecuteSwapContext<'info> {
     // Pocket accounts
@@ -226,100 +324,3 @@ impl<'info> ExecuteSwapContext<'info> {
     }
 }
 
-pub fn handle_execute_swap<'info>(ctx: Context<'_, '_, '_, 'info, ExecuteSwapContext<'info>>) -> Result<()> {
-    let pocket_registry = ctx.accounts.pocket_registry.clone();
-    let pocket = ctx.accounts.pocket.clone();
-    let signer = ctx.accounts.signer.clone();
-
-    // Only allow operator to perform the swap
-    if !pocket_registry.is_operator(signer.key()) {
-        return Err(PocketError::OnlyOperator.into());
-    }
-
-    // Check whether the pocket is ready to swap
-    if !pocket.is_ready_to_swap() {
-        return Err(PocketError::NotReadyToSwap.into());
-    }
-
-    // Make Swap
-    let did_swap = swap(&ctx).unwrap();
-
-    // Pocket risk check and update
-    ctx.accounts.ensure_pocket_integrity(&did_swap).unwrap();
-
-    // Return result
-    Ok(())
-}
-
-fn swap<'info>(ctx: &Context<'_, '_, '_, 'info, ExecuteSwapContext<'info>>) -> Result<DidSwap> {
-    let pocket = &ctx.accounts.pocket;
-
-    // Determine side
-    let mut side = Side::Ask;
-    if pocket.side == TradeSide::Buy {
-        side = Side::Bid;
-    }
-
-    let amount_to_swap = match pocket.side {
-        TradeSide::Buy => {
-            if pocket.batch_volume <= pocket.quote_token_balance {
-                pocket.batch_volume
-            } else {
-                pocket.quote_token_balance
-            }
-        }
-
-        TradeSide::Sell => {
-            if pocket.batch_volume <= pocket.base_token_balance {
-                pocket.batch_volume
-            } else {
-                pocket.base_token_balance
-            }
-        }
-    };
-
-    // Extract accounts
-    let event_queue = &mut ctx.remaining_accounts.get(0).unwrap();
-    let request_queue = &mut ctx.remaining_accounts.get(1).unwrap();
-    let market_bids = &mut ctx.remaining_accounts.get(2).unwrap();
-    let market_asks = &mut ctx.remaining_accounts.get(3).unwrap();
-    let coin_vault = &mut ctx.remaining_accounts.get(4).unwrap();
-    let pc_vault = &mut ctx.remaining_accounts.get(5).unwrap();
-    let market_authority = &mut ctx.remaining_accounts.get(6).unwrap();
-    let open_orders = &mut ctx.remaining_accounts.get(7).unwrap();
-    let dex_program = &mut ctx.remaining_accounts.get(8).unwrap();
-
-    // Make swap
-    let did_swap = external::swap(Swap {
-        market: MarketAccounts {
-            market: ctx.accounts.market_key.to_account_info(),
-            open_orders: open_orders.to_account_info(),
-            request_queue: request_queue.to_account_info(),
-            event_queue: event_queue.to_account_info(),
-            bids: market_bids.to_account_info(),
-            asks: market_asks.to_account_info(),
-            order_payer_token_account: match side {
-                Side::Bid => ctx.accounts.pocket_quote_token_vault.to_account_info(),
-                Side::Ask => ctx.accounts.pocket_base_token_vault.to_account_info(),
-            },
-            coin_wallet: ctx.accounts.pocket_base_token_vault.to_account_info(),
-            coin_vault: coin_vault.to_account_info(),
-            pc_vault: pc_vault.to_account_info(),
-            vault_signer: market_authority.to_account_info(),
-        },
-        authority: ctx.accounts.pocket.to_account_info(),
-        pc_wallet: ctx.accounts.pocket_quote_token_vault.to_account_info(),
-        dex_program: dex_program.to_account_info(),
-        token_program: ctx.accounts.token_program.to_account_info(),
-        rent: ctx.accounts.rent.to_account_info(),
-        pocket: ctx.accounts.pocket.clone(),
-    }, side, amount_to_swap, ExchangeRate {
-        rate: 0,
-        from_decimals: 0,
-        quote_decimals: 0,
-        strict: false,
-    }).unwrap();
-
-    // Return
-    Ok(did_swap)
-}
